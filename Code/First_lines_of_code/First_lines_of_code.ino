dd10c365b3699349fb1c7477854a8eafd4e1a8d0
CamilV <crv1g16@soton.ac.uk>
2017-11-06 15:12:48 +0000

    weird error correction

--- Code/First_lines_of_code/First_lines_of_code.ino
+++ Code/First_lines_of_code/First_lines_of_code.ino
@@ -1,32 +1,34 @@
-#include "robot.cpp"
+#include "defines.h"
 #include "ROBOT.h"
+
+Robot R;
 
 void setup() {
   
-}
+};
 
 void loop() {
-  Forward(856); // 13 to 11
-  Turn(0, 143, 1); // Spin on 11
-  Turn(180, 270, 0); // 11 to 10
-  Turn(0, 90, 0); // Spin 10
-  Forward(180); // 10 to 9
-  Turn(0, 140, 1); // Spin on 9
-  Forwad(622); // 9 to 8
-  Turn(0, 50, 1); // Spin on 8
-  Forward(400); // 8 to 7
-  Turn(0, 90, 1); //Spin on 7
-  Forward(400); // 7 to 6
-  Turn(0, 90, 1); // Spin on 6
-  Forward(400); // 6 to 5
-  Turn(0, 90, 1); // Spin on 5
-  Forward(660); // 5 to 4
-  Turn(0, 90, 0); // Spin on 4
-  Turn(260, 90 , 0); // 4 to 3
-  Turn(0, 90, 1); // Spin on 3
-  Forward(500); // 3 to 2
-  Turn(0, 90, 1); // Spin on 2
-  Forawrd(260) ; // 2 to 1
-  Turn(0, 90, 0); // Spin on 1
-  Forward(340); // 1 to 0
+  R.Forward(856); // 13 to 11
+  R.Turn(0, 143, 1); // Spin on 11
+  R.Turn(180, 270, 0); // 11 to 10
+  R.Turn(0, 90, 0); // Spin 10
+  R.Forward(180); // 10 to 9
+  R.Turn(0, 140, 1); // Spin on 9
+  R.Forward(622); // 9 to 8
+  R.Turn(0, 50, 1); // Spin on 8
+  R.Forward(400); // 8 to 7
+  R.Turn(0, 90, 1); //Spin on 7
+  R.Forward(400); // 7 to 6
+  R.Turn(0, 90, 1); // Spin on 6
+  R.Forward(400); // 6 to 5
+  R.Turn(0, 90, 1); // Spin on 5
+  R.Forward(660); // 5 to 4
+  R.Turn(0, 90, 0); // Spin on 4
+  R.Turn(260, 90 , 0); // 4 to 3
+  R.Turn(0, 90, 1); // Spin on 3
+  R.Forward(500); // 3 to 2
+  R.Turn(0, 90, 1); // Spin on 2
+  R.Forward(260) ; // 2 to 1
+  R.Turn(0, 90, 0); // Spin on 1
+  R.Forward(340); // 1 to 0
 }
