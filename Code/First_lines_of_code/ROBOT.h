1b664b99020e99398ade78b90cd0e7b81dd138ba
CamilV <crv1g16@soton.ac.uk>
2017-11-06 12:16:15 +0000

    recommit

--- Code/First_lines_of_code/ROBOT.h
+++ Code/First_lines_of_code/ROBOT.h
@@ -8,21 +8,20 @@ class Robot
   public:
     // Driving Functions
     void Forward(int distance);
-    void SpinLeft(float angle);
-    void SpinRight(float angle);
-    //void Turn(int ForwardSpeed, int TurnSpeed);
-    void Drive(int Speed);
     void Turn(float Radius,int Degrees, bool Direction);
-    void StraightTurn(float Radius, bool Direction, float Speed);
+    
     // Encoder Reading
     void ReadEncoders();
 
     // Initialize
     void Initialize();
     bool Goal;
+ 
   private:
     long E1, E2;
     float SumError;
+    void Drive(int Speed);
+    void StraightTurn(float Radius, bool Direction, float Speed);
     
 }
 ;
